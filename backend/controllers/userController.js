800dee9a3620b765489feba65387ac6037f625d4
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-09-24 13:53:29 +0530

    MC

---
+++ backend/controllers/userController.js
@@ -0,0 +1,12 @@
+
+const User = require('../models/User');
+
+exports.getUserProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    if (!user) return res.status(404).json({ message: 'User not found' });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
