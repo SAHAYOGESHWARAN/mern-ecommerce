5db68a6ee8a1ffd709cc7704319448dd5e46fe98
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-09-24 14:07:45 +0530

    ME

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,15 +1,16 @@
 
 const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 
-exports.isAuthenticated = (req, res, next) => {
+exports.isAuthenticated = async (req, res, next) => {
   const token = req.header('Authorization');
-  if (!token) return res.status(401).send('Access Denied');
+  if (!token) return res.status(401).json({ message: 'Unauthorized' });
 
   try {
-    const verified = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = verified;
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findById(decoded.id).select('-password');
     next();
   } catch (error) {
-    res.status(400).send('Invalid Token');
+    res.status(401).json({ message: 'Invalid token' });
   }
 };
