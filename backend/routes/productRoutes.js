f39989f250a0f47f806251bc13a5ceed747daea8
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-09-24 14:28:18 +0530

    ME

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,10 +1,12 @@
 
+
 const express = require('express');
-const { getProducts, createProduct } = require('../controllers/productController');
+const { createProduct, getAllProducts, getProductById } = require('../controllers/productController');
 const { isAuthenticated } = require('../middleware/authMiddleware');
 const router = express.Router();
 
-router.get('/', getProducts);
-router.post('/', isAuthenticated, createProduct);
+router.post('/', isAuthenticated, createProduct); // Auth required for adding a product
+router.get('/', getAllProducts);  // No auth required for fetching products
+router.get('/:id', getProductById);  // Fetch product by ID
 
 module.exports = router;
