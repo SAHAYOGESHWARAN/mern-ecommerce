a14ec6731422adac8b10277b2ba910ca89d55248
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-09-24 14:06:15 +0530

    ME

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,9 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
 import HomePage from './components/HomePage';
 import LoginPage from './components/LoginPage';
+import ProductDetail from './components/ProductDetail';
+import Cart from './components/Cart'; 
+
 
 const App = () => {
   return (
@@ -12,6 +15,8 @@ const App = () => {
       <Switch>
         <Route path="/" exact component={HomePage} />
         <Route path="/login" component={LoginPage} />
+        <Route path="/products/:id" component={ProductDetail} />
+        <Route path="/cart" component={Cart} /> 
       </Switch>
     </Router>
   );
